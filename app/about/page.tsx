fd9a87ffc110b7bb245604ebc2be10d33ef0bc66
Todor Cholakov <t.s.cholakov@gmail.com>
2024-05-08 23:57:19 +0200

    LL

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,13 @@
 export default function About() {
-
-    return (
-      <main className="flex justify-center items-center w-3xl ">
-      <div className=" bg-slate-400 ">aaaaaaaaaaaaaa saf asdfasdfasd fadf asdf asf fds asdfasd fasd fas fdf sa</div>
-      </main>
-    );
-  }
-  
+  return (
+    <main className="flex w-full justify-center ">
+      <div className=" w-[1024px] justify-left p-2 ">
+        <div className="w-96 mt-4">
+        <p  >Welcome to [Your Name]'s corner of the world, where every click captures a story! Meet [Your Name], the photography wizard specializing in [mention your areas of expertise]. They've got a knack for turning ordinary moments into extraordinary memories, sprinkling a bit of magic into each shot. Whether it's freezing time at a wedding or chasing sunsets for that perfect landscape, [Your Name] is your go-to person for all things photography. So, kick back, relax, and let's create some picture-perfect moments together!
+        </p>
+        </div>
+        
+      </div>
+    </main>
+  );
+}
